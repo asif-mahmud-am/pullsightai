463bbdc6c3625f08f3dc2482414982329268b37b
Md. Jubaidul Alam <jubaidul.ctg.bd@gmail.com>
2025-07-28 00:36:42 +0600

    refactor(BE): restrict CORS origins to specific domains

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,12 @@ import { AppModule } from './app.module'
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
     const options = {
-        origin: '*',
+        origin: [
+            'http://localhost:3000',
+            'http://localhost:3001',
+            'http://34.118.55.176:3000',
+            'http://34.118.55.176:3001'
+        ],
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
         preflightContinue: false,
         optionsSuccessStatus: 204,

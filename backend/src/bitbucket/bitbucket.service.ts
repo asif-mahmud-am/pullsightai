31391bd4aecf2e8473e5b514c46c05a3bd637231
Md. Jubaidul Alam <jubaidul.ctg.bd@gmail.com>
2025-08-22 12:15:49 +0600

    feat(BE): enhance event logging in processBitbucketEvent method

--- backend/src/bitbucket/bitbucket.service.ts
+++ backend/src/bitbucket/bitbucket.service.ts
@@ -154,14 +154,19 @@ export class BitbucketService {
     }
 
     async processBitbucketEvent(event: any, payload: any) {
+        const eventPayload = {
+            prTitle: payload.pullrequest.title,
+            prNumber: payload.pullrequest.id,
+            prState: payload.pullrequest.state
+        }
+        console.log(
+            `Processing Bitbucket event: ${event} ------->>`,
+            eventPayload
+        )
         await this.dataService.eventLogs.create({
             eventName: event,
             provider: 'bitbucket',
-            eventPayload: {
-                prTitle: payload.pullrequest.title,
-                prNumber: payload.pullrequest.id,
-                prState: payload.pullrequest.state
-            }
+            eventPayload
         })
         const isApplicable =
             await this.analysisService.checkApplicableForAnalysis(

11f0a31f80460db1027f6ae0ee754e7aed0121c3
Santu Roy <santuxroy@gmail.com>
2025-09-06 13:37:35 +0600

    fix(FE): showed proper message from BE while buying plan

--- frontend/src/app/app/subscription/CurrentPlan.tsx
+++ frontend/src/app/app/subscription/CurrentPlan.tsx
@@ -33,7 +33,7 @@ const CurrentPlan = () => {
     return (
         <div className="space-y-6">
             {/* Your current plan header */}
-            <div className="flex items-center justify-between gap-3">
+            <div className="flex flex-col lg:flex-row items-center justify-between gap-3">
                 <div className="mr-7">
                     <h2 className="text-xl font-semibold ">
                         Your current plan
